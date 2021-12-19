986778426d7a8ab77eebc8caef003c88dd141d94
robokae <alexanderhom19@gmail.com>
2021-12-18 17:32:16 -0800

    created scss files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WebFont from "webfontloader";
+import Home from "./pages/home/Home.js";
+import "./App.scss";
 
 function App() {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Inter"]
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/home/Home.js
@@ -0,0 +1,11 @@
+import "./Home.scss";
+
+function Home() {
+    return (
+        <div className="home-page-container">
+            <h1>Home page</h1>
+        </div>
+    );
+}
+
+export default Home;
