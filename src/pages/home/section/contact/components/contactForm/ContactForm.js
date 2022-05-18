a73190a55cc7f11b7af838fc549ccf5648d37f84
Alexander Hom <alexanderhom19@gmail.com>
2022-05-17 18:27:24 -0700

    update contact form to send data to backend

--- src/pages/home/section/contact/components/contactForm/ContactForm.js
+++ src/pages/home/section/contact/components/contactForm/ContactForm.js
@@ -1,9 +1,22 @@
+import axios from "axios";
 import { StyledButton } from "../../../../../../components/globalStyles";
 import "./ContactForm.scss";
 
 function ContactForm() {
+    const handleSubmitContactForm = (event) => {
+        event.preventDefault();
+        const url = "http://localhost:8080"
+        let dataToSend = {
+            "name": event.target.name.value,
+            "email": event.target.email.value,
+            "message": event.target.message.value
+        };
+
+        axios.post(`${url}/message`, dataToSend);
+    };
+
     return (
-        <form className="contact-form">
+        <form className="contact-form"  onSubmit={handleSubmitContactForm}>
             <div className="contact-form__input-container">
                 <label 
                     htmlFor="contact-form__name" 
@@ -11,7 +24,12 @@ function ContactForm() {
                 >
                     Name
                 </label>
-                <input type="text" id="contact-form__name" className="contact-form__input"/> 
+                <input 
+                    type="text" 
+                    id="contact-form__name" 
+                    className="contact-form__input"
+                    name="name"
+                /> 
             </div>
 
             <div className="contact-form__input-container">
@@ -21,7 +39,12 @@ function ContactForm() {
                 >
                     Email
                 </label>
-                <input type="text" id="contact-form__email" className="contact-form__input"/>
+                <input 
+                    type="text" 
+                    id="contact-form__email" 
+                    className="contact-form__input"
+                    name="email"
+                />
             </div>
 
             <div className="contact-form__input-container">
@@ -31,7 +54,11 @@ function ContactForm() {
                 >
                     Message
                 </label>
-                <textarea id="contact-form__message" className="contact-form__input contact-form__input--message"></textarea> 
+                <textarea 
+                    id="contact-form__message" 
+                    className="contact-form__input contact-form__input--message"
+                    name="message"
+                ></textarea> 
             </div>
 
             <StyledButton type="submit" className="contact-form__submit-button">Submit</StyledButton>
