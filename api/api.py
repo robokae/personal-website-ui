0339fb1d6a837a1129a76225abf8d6c72fa9c9e3
Alexander Hom <alexanderhom19@gmail.com>
2022-05-17 18:26:22 -0700

    switch to Java Spring Boot backend

--- api/api.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/message', methods=['POST'])
-def message(): 
-    return '<h1>My portfolio website</h1>'
---
+++ backend/personalwebsite/src/main/java/com/alexhom/personalwebsite/PersonalwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.alexhom.personalwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalwebsiteApplication.class, args);
+	}
+
+}
