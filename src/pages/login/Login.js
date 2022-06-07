c7a352ec9529d53b4d911e9b31ba15215a84ee03
Alexander Hom <alexanderhom19@gmail.com>
2022-06-07 09:42:48 -0700

    Add login handler

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,6 +1,23 @@
+import axios from 'axios';
 import './Login.scss';
 
 function Login() {
+    const handleLogin = (e) => {
+        const LOGIN_ENDPOINT = "/login";
+
+        // prevent page redirection
+        e.preventDefault();
+
+        // get login data from the form
+        let loginData = {
+            email: e.target.email.value,
+            password: e.target.password.value,
+        };
+
+        // send POST request to backend
+        axios.post(LOGIN_ENDPOINT, loginData);
+    }
+
     return (
         <div className="login-page">
             <div className="login-page__login-content">
@@ -31,6 +48,7 @@ function Login() {
                     <button 
                         type="submit" 
                         className="login-page__submit-button"
+                        onClick={handleLogin}
                     >
                         Submit
                     </button>
