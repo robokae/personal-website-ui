05aa9084428761a2407cf533e15ad0dbe2f018ae
Alexander Hom <alexanderhom19@gmail.com>
2022-09-03 19:19:37 -0700

    Minor style updates throughout home page

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <div className="footer-container">
             <div className="footer-container__content">
-                <p className="footer-container__text">Made with React, Sass, and Flask</p>
+                <p className="footer-container__text">Made with React, Sass, and Spring Boot</p>
                 <div className="footer-container__text-separator"></div>
                 <p className="footer-container__text">Alexander Hom 2022</p>
             </div>
