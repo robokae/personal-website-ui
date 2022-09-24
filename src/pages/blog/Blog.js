02b779c6bb3b7db8034bdf6bed6780966a50a7d9
Alexander Hom <alexanderhom19@gmail.com>
2022-09-24 10:33:32 -0700

    Add initial blog page styling

--- src/pages/blog/Blog.js
+++ src/pages/blog/Blog.js
@@ -1,11 +1,11 @@
 import "./Blog.scss";
 
 function Blog() {
-    return (
-        <div className="blog-page-container">
-            <h1>Blog page</h1>
-        </div>
-    );
+  return (
+    <div className="blog-page">
+      <h1>Blog page</h1>
+    </div>
+  );
 }
 
 export default Blog;
