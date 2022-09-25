5c835703cc5f3efd67f0191a1d2c297934402e4e
Alexander Hom <alexanderhom19@gmail.com>
2022-09-24 21:21:06 -0700

    Update footer content

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,16 +1,18 @@
 import "../../scss/layout/_footer.scss";
 
 function Footer() {
-    return (
-        <div className="footer-container">
-            <div className="footer-container__content">
-                <p className="footer-container__text">Made with React, Sass, and Spring Boot</p>
-                <div className="footer-container__text-separator">&#8226;</div>
-                {/* &#8226; */}
-                <p className="footer-container__text">Alexander Hom 2022</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="footer-container">
+      <div className="footer-container__content">
+        <p className="footer-container__text">Made with React and Sass</p>
+        <div className="footer-container__text-separator">&#8226;</div>
+        {/* &#8226; */}
+        <p className="footer-container__text">
+          Alexander Hom {new Date().getFullYear()}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
