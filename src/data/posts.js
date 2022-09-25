feb06d2b40ac9439d463491f9bc878c3363129ac
Alexander Hom <alexanderhom19@gmail.com>
2022-09-25 11:55:16 -0700

    Add first blog post data

---
+++ src/data/posts.js
@@ -0,0 +1,11 @@
+const posts = [
+  {
+    title: "My First Blog Post",
+    publishDate: "September 25, 2022",
+    description:
+      "Hello world! I'm very excited to announce that this is my first post on my blog.",
+    tags: [],
+  },
+];
+
+export default posts;
--- src/data/resume.js
+++
@@ -1,3 +0,0 @@
-const resume = [];
-
-export default resume;
