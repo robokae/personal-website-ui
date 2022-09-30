8f14cd38475021741bf0073489a4ebc773c18a6c
Alexander Hom <alexanderhom19@gmail.com>
2022-09-29 21:41:14 -0700

    Add blog button to slide out menu

--- src/components/slideOutMenu/SlideOutMenu.js
+++ src/components/slideOutMenu/SlideOutMenu.js
@@ -4,35 +4,48 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import "./SlideOutMenu.scss";
 
 function SlideOutMenu(props) {
-    const { display, setDisplay, toggleTheme } = props;
-    
-    return (
-        <div className={`slide-out-menu ${display ? "slide-out-menu--display" : ""}`}>
-            <div className="slide-out-menu__content">
-                <div className="slide-out-menu__top">
-                    <FontAwesomeIcon 
-                        className="slide-out-menu__close-button" 
-                        icon={faTimes} 
-                        onClick={() => setDisplay(false)}
-                    />
-                </div>
-                <h2 className="slide-out-menu__heading">Menu</h2>
-                <div className="slide-out-menu__bottom">
-                    <ul className="slide-out-menu__options">
-                        <li className="slide-out-menu__option">
-                            <NavLink to="/" className="slide-out-menu__link">Home</NavLink>
-                        </li> 
-                        <li className="slide-out-menu__option">
-                            <NavLink to="/#contact" className="slide-out-menu__link">Contact</NavLink>
-                        </li>
-                        <li className="slide-out-menu__option">
-                            <p className="slide-out-menu__option-text" onClick={toggleTheme}>Toggle Theme</p>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+  const { display, setDisplay, toggleTheme } = props;
+
+  return (
+    <div
+      className={`slide-out-menu ${display ? "slide-out-menu--display" : ""}`}
+    >
+      <div className="slide-out-menu__content">
+        <div className="slide-out-menu__top">
+          <FontAwesomeIcon
+            className="slide-out-menu__close-button"
+            icon={faTimes}
+            onClick={() => setDisplay(false)}
+          />
         </div>
-    );
+        <h2 className="slide-out-menu__heading">Menu</h2>
+        <div className="slide-out-menu__bottom">
+          <ul className="slide-out-menu__options">
+            <li className="slide-out-menu__option">
+              <NavLink to="/" className="slide-out-menu__link">
+                Home
+              </NavLink>
+            </li>
+            <li className="slide-out-menu__option">
+              <NavLink to="/blog" className="slide-out-menu__link">
+                Blog
+              </NavLink>
+            </li>
+            <li className="slide-out-menu__option">
+              <NavLink to="/#contact" className="slide-out-menu__link">
+                Contact
+              </NavLink>
+            </li>
+            <li className="slide-out-menu__option">
+              <p className="slide-out-menu__option-text" onClick={toggleTheme}>
+                Toggle Theme
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SlideOutMenu;
