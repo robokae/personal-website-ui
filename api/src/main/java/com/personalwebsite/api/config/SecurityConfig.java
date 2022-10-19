b9010eb52f289abcee2b2169c252e1c9af8d0109
Alexander Hom <alexanderhom19@gmail.com>
2022-10-18 19:48:05 -0700

    Add new Beans to SecurityConfig

--- api/src/main/java/com/personalwebsite/api/config/SecurityConfig.java
+++ api/src/main/java/com/personalwebsite/api/config/SecurityConfig.java
@@ -1,20 +1,47 @@
 package com.personalwebsite.api.config;
 
+import com.personalwebsite.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http = http.cors().and().csrf().disable();
+public class SecurityConfig {
 
-        http.authorizeRequests()
-            .anyRequest()
-            .permitAll();
+    @Autowired
+    private UserRepository userRepository;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/**").hasRole("USER")
+                .and()
+                .csrf()
+                .disable()
+                .build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findByUsername(username);
+            }
+        };
     }
 }
--- api/src/main/java/com/personalwebsite/api/repository/UserRepository.java
+++ api/src/main/java/com/personalwebsite/api/repository/UserRepository.java
@@ -5,5 +5,5 @@ import com.personalwebsite.api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    User findByUsername(String username);
 }
