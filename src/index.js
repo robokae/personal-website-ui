39c46d7552d4915a13be5b5a923629311c19556e
Alexander Hom <alexanderhom19@gmail.com>
2024-07-14 17:44:00 -0700

    Install Redux Toolkit and create global store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./scss/main.scss";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
