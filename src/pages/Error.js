ecd20b5e50a5593d1ab2554d4c0b5e428b02dd03
Alexander Hom <alexanderhom19@gmail.com>
2024-12-14 14:04:31 -0800

    Refactor error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,56 +1,23 @@
 import styled from "styled-components";
-import header from "content/layout/header.json";
-import footer from "content/layout/footer.json";
-import hamburgerMenu from "content/hamburgerMenu.json";
-import Header from "components/layout/header/Header";
-import Footer from "components/footer/Footer";
 import error from "content/error/error.json";
 import { LinkButton } from "components/Link";
-import { Layout } from "constants/layout";
-import HamburgerMenu from "components/menu/hamburgerMenu/HamburgerMenu";
-
-const Container = styled.main`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100vh;
-`;
-
-const ContentContainer = styled.section`
-  width: 100%;
-  top: ${Layout.HEADER_HEIGHT};
-  padding-top: 3rem;
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 2.5rem;
-`;
+import { PageContentLayout, SectionLayout } from "components/layout/Layout";
+import Typography from "components/typography/Typography";
+import { Typography as TypographyConstants } from "constants/typography";
 
 const { content } = error;
 
-function Error({ theme, onChangeTheme }) {
+function Error() {
   return (
-    <Container>
-      <Header
-        links={header.links}
-        isActive={false}
-        theme={theme}
-        onChangeTheme={onChangeTheme}
-        changeBgOnScroll={false}
-      />
-      <HamburgerMenu
-        links={hamburgerMenu.links}
-        theme={theme}
-        onChangeTheme={onChangeTheme}
-      />
-      <ContentContainer>
-        <h2>{content.heading}</h2>
+    <PageContentLayout>
+      <SectionLayout>
+        <Typography tag={TypographyConstants.H2} textAlign="center">
+          {content.heading}
+        </Typography>
         <p>{content.text}</p>
         <LinkButton to={content.ctaUrl}>{content.ctaText}</LinkButton>
-      </ContentContainer>
-      <Footer textContent={footer.text} />
-    </Container>
+      </SectionLayout>
+    </PageContentLayout>
   );
 }
 
