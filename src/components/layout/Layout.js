a59486e338b892301b2f00484a66a0c0e912512f
Alexander Hom <alexanderhom19@gmail.com>
2024-12-16 19:43:35 -0800

    Clean up Layout.js

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,4 +1,4 @@
-import { Layout } from "constants/layout";
+import { Breakpoint, Layout } from "constants/layout";
 import styled from "styled-components";
 
 export const BaseLayout = styled.div`
@@ -9,34 +9,24 @@ export const BaseLayout = styled.div`
 `;
 
 export const PageContentLayout = styled(BaseLayout)`
-  min-height: 100vh;
   margin-top: ${Layout.HEADER_HEIGHT};
 `;
 
 export const ContentLayout = styled(BaseLayout)`
-  width: 100%;
   max-width: ${Layout.CONTENT_MAX_WIDTH};
   gap: ${Layout.GAP_LG};
 `;
 
 export const HeroLayout = styled(BaseLayout)`
   margin-top: -${Layout.HEADER_HEIGHT};
-  padding: ${Layout.SECTION_PADDING} 0;
-  padding-bottom: 0;
 `;
 
-export const SectionLayout = styled.section`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+export const SectionLayout = styled(BaseLayout)`
   padding: ${Layout.SECTION_PADDING};
   gap: ${Layout.GAP_LG};
   position: relative;
-`;
 
-export const TwoPaneLayout = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: 20% 80%;
+  @media ${Breakpoint.MOBILE_LG} {
+    padding: ${Layout.SECION_PADDING_SM};
+  }
 `;
